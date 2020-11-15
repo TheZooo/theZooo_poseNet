4633314c5355f387ca9d9f28f2c35f9cb4975377
Eric Zhou <42453465+TheZooo@users.noreply.github.com>
2020-11-15 12:20:13 -1000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,25 +1,34 @@
-const width = 480;
-const height = 360;
+const width = window.innerWidth;
+const height = window.innerHeight;
+const videoWidth = 480;
+const videoHeight = 360;
 
 let video;
 let poseNet;
 let pose;
 
+var lookScriptBool = true;
+var facePntBool = false;
+var visAimBool = false;
+var visMpBool = false;
+var laserEyesBool = true;
+
 function setup() {
     //Usual Canvas Setup
     createCanvas(width, height);
+    //document.getElementsByTagName('canvas')[0].style.transform = 'translate(' + -videoWidth/2 + 'px,' + -videoHeight/2 + 'px)';
     //Creating capture using only video (p5js)
     video = createCapture(VIDEO);
-    video.size(width, height);
+    video.size(videoWidth, videoHeight);
     video.hide();
-    //Adding poseNet and sending HTMLVideoElement to video; callback:modelLoaded
+    //Adding poseNet and sending HTMLVideoElement to video; callback: modelLoaded()
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses); //Initiates when a "pose" is detected;  callback: gotPoses(poses)
 }
 
 function gotPoses(poses) {
     //poses are the people it detects on video
-    console.log(poses);
+    //console.log(poses);
 
     if (poses.length > 0) {
         pose = poses[0].pose;
@@ -30,13 +39,48 @@ function modelLoaded() {
     console.log('Model Loaded!');
 }
 
+function toggle(thing) {
+    switch(thing) {
+        case 'lookScrp':
+            lookScriptBool = !lookScriptBool;
+        break;
+        case 'facepnts':
+            facePntBool = !facePntBool;
+        break;
+        case 'aimer':
+            visAimBool = !visAimBool;
+        break;
+        case 'mpAvg':
+            visMpBool = !visMpBool;
+        break;
+        case 'lsrEyes':
+            laserEyesBool = !laserEyesBool;
+        break;
+    }
+}
+
 function draw() {
-    translate(video.width, 0);
+    background(255);
+    translate(width/2 + videoWidth/2, height/2 - videoHeight/2);
     scale(-1, 1);
-    image(video, 0, 0, width, height);
+    image(video, 0, 0, videoWidth, videoHeight);
 
     if (pose) {
-        drawFacePoints();
+        if (facePntBool) {
+            drawFacePoints();
+        }
+        if (lookScriptBool) {
+            findCenterFace();
+            if (visAimBool) {
+                drawLookPoint();
+            }
+            if (visMpBool) {
+                drawCenterPoint();
+            }
+        }
+    }
+    if (laserEyesBool) {
+        LASERREYESSSS();
     }
 }
 
@@ -48,7 +92,7 @@ function drawFacePoints() {
         if (score > 0.2) {
             fill(255);
             stroke(0);
-            strokeWeight(4);
+            strokeWeight(2);
             ellipse(x, y, 16, 16);
         }
     }
